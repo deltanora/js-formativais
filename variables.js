8af42ddebe0ddeadd73fe7317612896eb2474712
Your Name <you@example.com>
2025-10-20 09:16:45 +0300

    variables.js

---
+++ variables.js
@@ -0,0 +1,17 @@
+let groupName= "PT2025";
+  console.log(groupName);
+
+  let num = 23;
+  console.log(num);
+
+  let  isActive=2
+  if (isActive <=1){
+      console.log("Ir patiesa");
+  }else{
+    console.log("Nav patiess");
+  }
+
+  const EULER = 2.718;
+  console.log(EULER);
+
+  
